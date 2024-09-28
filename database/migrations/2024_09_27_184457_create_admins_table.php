a2e3b42e14e4877f8d5184db96351155d21a3e7e
Akhdan <akhdannajiib@gmail.com>
2024-09-28 14:47:21 +0700

    add table

--- database/migrations/2024_09_27_184457_create_admins_table.php
+++ database/migrations/2024_09_27_184457_create_admins_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('username')->unique();
-            $table->string('password')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
